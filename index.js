7f2015bc3fa5757f00f49c16e29d7968e15b0c78
Zaurbek Zhakupov <bigz3733@gmail.com>
2017-05-26 23:16:02 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).on('ready', function() {  
+  var winHeight = $(window).height(), 
+      docHeight = $(document).height(),
+      progressBar = $('progress'),
+      max, value;
+
+  max = docHeight + winHeight*0.8;
+  progressBar.attr('max', max);
+
+  $(document).on('scroll', function(){
+     value = $(window).scrollTop();
+     progressBar.attr('value', value);
+  });
+});
